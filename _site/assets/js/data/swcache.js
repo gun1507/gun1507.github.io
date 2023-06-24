c98733a8cd25f766b42d4b1db95ec6cad81e52b8
gun1507 <mario.kwon@kakaocorp.com>
2023-06-25 02:34:01 +0900

    update - test

--- _site/assets/js/data/swcache.js
+++ _site/assets/js/data/swcache.js
@@ -1,60 +1,53 @@
 const resource = [
+    /* --- CSS --- */
+    '/assets/css/style.css',
 
-  /* --- CSS --- */
-  '/assets/css/style.css',
+    /* --- PWA --- */
+    '/app.js',
+    '/sw.js',
 
-  /* --- JavaScripts --- */
-  
-  '/assets/js/dist/home.min.js',
-  '/assets/js/dist/page.min.js',
-  '/assets/js/dist/post.min.js',
-  '/assets/js/dist/categories.min.js',
-  '/assets/js/data/search.json',
-  '/app.js',
-  '/sw.js',
+    /* --- HTML --- */
+    '/index.html',
+    '/404.html',
 
-  /* --- HTML --- */
-  '/index.html',
-  '/404.html',
-  
-    '/categories/',
-  
-    '/tags/',
-  
-    '/archives/',
-  
-    '/about/',
-  
+    
+        '/categories/',
+    
+        '/tags/',
+    
+        '/archives/',
+    
+        '/about/',
+    
 
-  /* --- Favicons --- */
-  
-
-  '/assets/img/favicons/android-chrome-192x192.png',
-  '/assets/img/favicons/android-chrome-512x512.png',
-  '/assets/img/favicons/apple-touch-icon.png',
-  '/assets/img/favicons/favicon-16x16.png',
-  '/assets/img/favicons/favicon-32x32.png',
-  '/assets/img/favicons/favicon.ico',
-  '/assets/img/favicons/mstile-150x150.png',
-  '/assets/img/favicons/site.webmanifest',
-  '/assets/img/favicons/browserconfig.xml'
-
+    /* --- Favicons & compressed JS --- */
+    
+    
+        '/assets/img/favicons/android-chrome-192x192.png',
+        '/assets/img/favicons/android-chrome-512x512.png',
+        '/assets/img/favicons/apple-touch-icon.png',
+        '/assets/img/favicons/favicon-16x16.png',
+        '/assets/img/favicons/favicon-32x32.png',
+        '/assets/img/favicons/favicon.ico',
+        '/assets/img/favicons/mstile-150x150.png'
 ];
 
 /* The request url with below domain will be cached */
 const allowedDomains = [
-  
+    
 
-  'localhost:4000',
+    'localhost:4000',
 
-  'fonts.gstatic.com',
-  'fonts.googleapis.com',
-  'cdn.jsdelivr.net',
-  'polyfill.io'
+    
+        'chirpy-img.netlify.app',
+    
+
+    'fonts.gstatic.com',
+    'fonts.googleapis.com',
+    'cdn.jsdelivr.net',
+    'polyfill.io'
 ];
 
 /* Requests that include the following path will be banned */
-const denyUrls = [
-  
-];
+const denyUrls = [];
 
