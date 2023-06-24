4c42c1ba4a7747a38c7608431ef8b164bbc1b967
gun1507 <mario.kwon@kakaocorp.com>
2023-06-25 02:13:15 +0900

    update - test

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230625.0203';
+const cacheName = 'chirpy-20230625.0212';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
